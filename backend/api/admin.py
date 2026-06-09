80c918d755022f2e14d9be1fbb3dfb7de4501191
Tapandat <166542132+Tapandat@users.noreply.github.com>
2026-06-09 16:39:10 +0530

    Add logs endpoint and format user actions

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,55 +1,66 @@
 from fastapi import APIRouter
 
+from backend.services.activity_logger import (
+get_logs
+)
+
 from backend.services.user_service import (
-    get_all_users,
-    delete_user,
-    promote_user,
-    block_user,
-    unblock_user
+get_all_users,
+delete_user,
+promote_user,
+block_user,
+unblock_user
 )
 
 router = APIRouter(
-    prefix="/admin",
-    tags=["admin"]
+prefix="/admin",
+tags=["admin"]
 )
 
+@router.get("/logs")
+def logs():
+return get_logs()
 
 @router.get("/users")
 def users():
-    return get_all_users()
-
+return get_all_users()
 
 @router.delete("/users/{email}")
 def remove_user(email: str):
-    delete_user(email)
+delete_user(email)
 
-    return {
-        "message": "User deleted"
-    }
-
+```
+return {
+    "message": "User deleted"
+}
+```
 
 @router.put("/promote/{email}")
 def promote(email: str):
-    promote_user(email)
+promote_user(email)
 
-    return {
-        "message": "User promoted"
-    }
-
+```
+return {
+    "message": "User promoted"
+}
+```
 
 @router.put("/block/{email}")
 def block(email: str):
-    block_user(email)
+block_user(email)
 
-    return {
-        "message": "User blocked"
-    }
-
+```
+return {
+    "message": "User blocked"
+}
+```
 
 @router.put("/unblock/{email}")
 def unblock(email: str):
-    unblock_user(email)
+unblock_user(email)
 
-    return {
-        "message": "User unblocked"
-    }
+```
+return {
+    "message": "User unblocked"
+}
+```
